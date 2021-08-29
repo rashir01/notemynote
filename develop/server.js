fddefb6204961076643ef44b8d4e60e540d5b211
Ray Ashir <rayeed.ashir@gmail.com>
2021-08-28 22:38:48 -0700

    added get notes and get *

--- develop/server.js
+++ develop/server.js
@@ -1,2 +1,63 @@
 const express = require('express');
+const path = require('path');
 
+const PORT = process.env.port || 3001;
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/notes.html'));
+  console.log('get: /notes ');
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+})
+
+app.listen(PORT, () =>
+  console.log(`Example app listening at http://localhost:${PORT}`)
+);
+
+/* DONE 
+GET /notes should return the notes.html file.
+GET * should return the index.html file.
+*/
+
+/*
+Getting Started
+On the back end, the application should include a db.json file that will be used to store and retrieve notes using the fs module.
+
+The following HTML routes should be created:
+
+
+
+
+
+The following API routes should be created:
+
+GET /api/notes should read the db.json file and return all saved notes as JSON.
+
+POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
+
+Bonus
+You haven’t learned how to handle DELETE requests, but this application offers that functionality on the front end. As a bonus, try to add the DELETE route to the application using the following guideline:
+
+DELETE /api/notes/:id should receive a query parameter that contains the id of a note to delete. To delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file.
+*/
+/*
+GIVEN a note-taking application
+WHEN I open the Note Taker
+THEN I am presented with a landing page with a link to a notes page
+WHEN I click on the link to the notes page
+THEN I am presented with a page with existing notes listed in the left-hand column, plus empty fields to enter a new note title and the note’s text in the right-hand column
+WHEN I enter a new note title and the note’s text
+THEN a Save icon appears in the navigation at the top of the page
+WHEN I click on the Save icon
+THEN the new note I have entered is saved and appears in the left-hand column with the other existing notes
+WHEN I click on an existing note in the list in the left-hand column
+THEN that note appears in the right-hand column
+WHEN I click on the Write icon in the navigation at the top of the page
+THEN I am presented with empty fields to enter a new note title and the note’s text in the right-hand column
+*/
