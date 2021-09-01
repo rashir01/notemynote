45820cefedefeb460d867e090675e47913373f92
Ray Ashir <rayeed.ashir@gmail.com>
2021-08-31 22:36:52 -0700

    remove dead comments

--- develop/server.js
+++ develop/server.js
@@ -4,8 +4,6 @@ const notes = require('./db/db.json');
 const { v4: uuidv4 } = require('uuid');
 const fs = require('fs');
 
-
-
 const PORT = process.env.PORT || 3001;
 
 const app = express();
@@ -14,11 +12,13 @@ const app = express();
 app.use(express.json());
 app.use(express.static('public'))
 
+//GET /notes routh
 app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, './public/notes.html'));
   console.log('servign notes ');
 });
 
+//GET /api/notes routs. returns all notes in json
 app.get('/api/notes', (req, res) => {
   console.log("api/nots served");
   fs.readFile('./db/db.json', 'utf8', (err, data) => {
@@ -82,56 +82,3 @@ app.post('/api/notes', (req, res) => {
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
 );
-
-/*current
-
-
-
-*/
-
-/* DONE 
-GET /notes should return the notes.html file.
-GET * should return the index.html file.
-GET /api/notes should read the db.json file and return all saved notes as JSON.
-POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
-*/
-
-/*
-
-
-/* todo
-
-Bonus
-You haven’t learned how to handle DELETE requests, but this application offers that functionality on the front end. As a bonus, try to add the DELETE route to the application using the following guideline:
-
-DELETE /api/notes/:id should receive a query parameter that contains the id of a note to delete. To delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file.
-*/
-/*
-GIVEN a note-taking application
-WHEN I open the Note Taker
-THEN I am presented with a landing page with a link to a notes page
-----already done if you start the server
-
-
-WHEN I click on the link to the notes page
-THEN I am presented with a page with existing notes listed in the left-hand column, plus empty fields to enter a new note title and the note’s text in the right-hand column
----should be done as well once server is done
-
-
-WHEN I enter a new note title and the note’s text
-THEN a Save icon appears in the navigation at the top of the page
----should already be done
-
-
-WHEN I click on the Save icon
-THEN the new note I have entered is saved and appears in the left-hand column with the other existing notes
----should already be done (maybe using Post?
-  )
-WHEN I click on an existing note in the list in the left-hand column
-THEN that note appears in the right-hand column
---- already done
-
-WHEN I click on the Write icon in the navigation at the top of the page
-THEN I am presented with empty fields to enter a new note title and the note’s text in the right-hand column
-----should already be done
-*/
